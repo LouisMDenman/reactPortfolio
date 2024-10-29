ed05f226dd9dfc4e53e63dfeda394a58f83c6f40
LouisMDenman <lmdenman02@gmail.com>
2024-10-29 15:35:48 +1000

    Added and sized website sections.

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,9 @@
+
+
+export function Contact() {
+    return (
+        <div id="contact">
+            <h1>Hello, this is the contact me page</h1>
+        </div>
+    )
+}
---
+++ src/sections/Experience.jsx
@@ -0,0 +1,9 @@
+
+
+export function Experience() {
+    return (
+        <div id="experience">
+            <h1>Hello, this is the experience page</h1>
+        </div>
+    )
+}
