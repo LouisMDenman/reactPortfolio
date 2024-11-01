8997a7f45b972ad6251644a53957ea77a7c6122a
LouisMDenman <lmdenman02@gmail.com>
2024-11-01 10:11:30 +1000

    implemented function navbar, along with placeholder css

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import './styles/About.css'
 import './styles/Experience.css'
 import './styles/Projects.css'
 import './styles/Contact.css'
+import './styles/Navbar.css'
 import { useThemeContext } from './contexts/ThemeContextProvider'
 import { HomePage } from './sections/HomePage'
 import { About } from './sections/About'
 import { Experience } from './sections/Experience'
 import { Projects } from './sections/Projects'
 import { Contact } from './sections/Contact'
+import { Navbar } from './components/Navbar'
 
 function App() {
   const [currentTheme, toggleTheme] = useThemeContext()
 
   return (
     <>
+      <Navbar />
       <HomePage />
       <About />
       <Experience />
