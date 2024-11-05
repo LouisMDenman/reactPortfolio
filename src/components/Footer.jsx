66e773d091be5691ac3319dfbfacea13091ab8f9
LouisMDenman <lmdenman02@gmail.com>
2024-11-05 12:11:17 +1000

    added contact me page content and styled with css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@
 export function Footer() {
     return (
         <div id="footer">
-            <h3 id="footerMessage">Designed & Built by Louis Denman {(new Date().getFullYear())}</h3>
+            <h3 id="footerMessage">Designed & Built by <a>Louis Denman</a> {(new Date().getFullYear())}</h3>
         </div>
     )
 }
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -3,7 +3,12 @@
 export function Contact() {
     return (
         <div id="contact">
-            <h1>Hello, this is the contact me page</h1>
+            <h2 id="contactHeader">Contact Me</h2>
+            <p id="contactPara">As an emerging developer in 2025, I am always happy to expand my network and meet other people who share the same passion. In the links below you can either view my LinkedIn and reach out, or alternatively view some of my projects on my github account.</p>
+            <div id="contactLinks">
+                <a href="https://github.com/LouisMDenman"><img src="src/assets/github.png" alt="github icon link" class="contactimages" /></a>
+                <a href="https://au.linkedin.com/in/louis-denman-2b441a2a4"><img src="src/assets/linkedin.png" alt="linkedin icon link" class="contactimages" /></a>
+            </div>
         </div>
     )
 }
