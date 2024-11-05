98a68b6941740c503afb7c9a92275eab517a911f
LouisMDenman <lmdenman02@gmail.com>
2024-11-05 15:07:41 +1000

    implemented projects section and styled with css.

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,6 +4,36 @@ export function Projects() {
     return (
         <div id="projects">
             <h2 id="projectsHeader">Main Projects</h2>
+            <br></br>
+            <div class="projectDiv">
+                <div class="projectContent">
+                    <h3>Doldrum Disruptor</h3>
+                    <p>Doldrum Disruptor is a website that is built with React, and makes use of the Bored API to generate activity suggestions based on a number of participants and categories. It was created within a week for the Coder Academy Hackathon 2024.
+                    </p>
+                    <a id="link">https://github.com/LouisMDenman/doldrumDisruptor</a>
+                </div>
+                <img src="src/assets/dd.png" class="projectImg"/>
+            </div>
+            <br></br>
+            <br></br>
+            <div class="projectDiv">
+                <div class="projectContent">
+                    <h3>Spotify Dashboard</h3>
+                    <p>This spotify dashboard is another website that is built with React, and makes use of the spotify API to display the top 20 tracks from my personal account. The project involved learning more about account verification and validation, as well as the use of APIs.</p>
+                    <a id="link">https://github.com/LouisMDenman/spotifyDashboard</a>
+                </div>
+                <img src="src/assets/sd.png" class="projectImg"/>
+            </div>
+            <br></br>
+            <br></br>
+            <div class="projectDiv">
+                <div class="projectContent">
+                    <h3>Previous Portfolio</h3>
+                    <p>This personal portfolio is made purely with HTML and CSS, and whilst slightly outdated was a foundational learning block for front-end development concepts. It has since been replaced, obviously, by this current portfolio which was built with React in Ubuntu OS.</p>
+                    <a id="link">https://github.com/LouisMDenman/portfolio</a>
+                </div>
+                <img src="src/assets/pp.png" class="projectImg"/>
+            </div>
         </div>
     )
 }
