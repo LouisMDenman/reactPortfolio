10c7bdd2679929c78c38cf39f8c9a2b0b67734eb
LouisMDenman <lmdenman02@gmail.com>
2024-11-06 12:24:42 +1000

    added href to project links that was forgotten

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -10,7 +10,7 @@ export function Projects() {
                     <h3>Doldrum Disruptor</h3>
                     <p>Doldrum Disruptor is a website that is built with React, and makes use of the Bored API to generate activity suggestions based on a number of participants and categories. It was created within a week for the Coder Academy Hackathon 2024.
                     </p>
-                    <a class="link">https://github.com/LouisMDenman/doldrumDisruptor</a>
+                    <a class="link" href="https://github.com/LouisMDenman/doldrumDisruptor">https://github.com/LouisMDenman/doldrumDisruptor</a>
                 </div>
                 <img src="/assets/dd.png" class="projectImg"/>
             </div>
@@ -20,7 +20,7 @@ export function Projects() {
                 <div class="projectContent">
                     <h3>Spotify Dashboard</h3>
                     <p>This spotify dashboard is another website that is built with React, and makes use of the spotify API to display the top 20 tracks from my personal account. The project involved learning more about account verification and validation, as well as the use of APIs.</p>
-                    <a class="link">https://github.com/LouisMDenman/spotifyDashboard</a>
+                    <a class="link" href="https://github.com/LouisMDenman/spotifyDashboard">https://github.com/LouisMDenman/spotifyDashboard</a>
                 </div>
                 <img src="/assets/sd.png" class="projectImg"/>
             </div>
@@ -30,7 +30,7 @@ export function Projects() {
                 <div class="projectContent">
                     <h3>Previous Portfolio</h3>
                     <p>This personal portfolio is made purely with HTML and CSS, and whilst slightly outdated was a foundational learning block for front-end development concepts. It has since been replaced, obviously, by this current portfolio which was built with React in Ubuntu OS.</p>
-                    <a class="link">https://github.com/LouisMDenman/portfolio</a>
+                    <a class="link" href="https://github.com/LouisMDenman/portfolio">https://github.com/LouisMDenman/portfolio</a>
                 </div>
                 <img src="/assets/pp.png" class="projectImg"/>
             </div>
