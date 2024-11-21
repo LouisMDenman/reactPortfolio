fe96ca3d949413e062e197234423df95f3a7b72a
Louis Denman <88065644+LouisMDenman@users.noreply.github.com>
2024-11-21 10:01:56 +1000

    Updated new linkedin link in contact me page

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -7,7 +7,7 @@ export function Contact() {
             <p id="contactPara">As an emerging developer in 2025, I am always happy to expand my network and meet other people who share the same passion. In the links below you can either view my LinkedIn and reach out, or alternatively view some of my projects on my github account.</p>
             <div id="contactLinks">
                 <a href="https://github.com/LouisMDenman"><img src="assets/github.png" alt="github icon link" class="contactimages" /></a>
-                <a href="https://au.linkedin.com/in/louis-denman-2b441a2a4"><img src="assets/linkedin.png" alt="linkedin icon link" class="contactimages" /></a>
+                <a href="https://www.linkedin.com/in/louisdenman/"><img src="assets/linkedin.png" alt="linkedin icon link" class="contactimages" /></a>
             </div>
         </div>
     )
